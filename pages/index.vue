faeaf1679a750ad4a36cb1377d858104a07acaef
tefo <tefo.ha27@gmail.com>
2021-06-25 23:53:14 +0430

    24- refetch data if not avalible

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <p>profile page</p>
+  <div v-if="!isError">
+    <p>profile page</p>
+  </div>
+  <p v-else>مشکلی پیش آمد. دوباره سعی کنید.</p>
 </template>
 
 <script>
-export default {}
+export default {
+  async fetch() {
+    try {
+      await this.$store.dispatch('getForm')
+    } catch (error) {
+      this.isError = true
+    }
+  },
+  data: () => ({
+    isError: false,
+  }),
+}
 </script>
