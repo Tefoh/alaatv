6be704b134fd12fe3d3794d2f55737d73df5e2f5
tefo <tefo.ha27@gmail.com>
2021-06-26 01:08:03 +0430

    26- rest profile form

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <v-card-text>
         <p class="text-h4 text--primary">فرم ویرایش پروفایل</p>
 
-        <v-form ref="loginForm" @submit.prevent="handleSubmit">
+        <v-form ref="profileForm" @submit.prevent="handleSubmit">
           <v-row>
             <v-col cols="12" md="6">
               <label
@@ -26,13 +26,13 @@
 
             <v-col cols="12" md="6">
               <label
-                for="code-melli"
+                for="last_name"
                 class="subtitle-1 gray--text text--darken-2"
               >
                 نام خانوادگی
               </label>
               <v-text-field
-                id="code-melli"
+                id="last_name"
                 v-model="form.last_name"
                 class="mt-2"
                 outlined
@@ -66,10 +66,102 @@
                 v-model="form.school"
                 class="mt-2"
                 outlined
-                label="محمدی"
+                label="گل محمدی"
                 prepend-inner-icon="mdi-school"
                 :rules="[(v) => required(v, 'مدرسه')]"
               ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="6">
+              <label for="major" class="subtitle-1 gray--text text--darken-2">
+                رشته
+              </label>
+
+              <v-select
+                id="major"
+                v-model="form.major_id"
+                :items="$store.state.majors.majors"
+                item-text="name"
+                item-value="id"
+                label="رشته خود را انتخاب کنید"
+                :rules="[(v) => required(v, 'رشته')]"
+                outlined
+              ></v-select>
+            </v-col>
+
+            <v-col cols="12" md="6">
+              <label for="grade" class="subtitle-1 gray--text text--darken-2">
+                مقطع تحصیلی
+              </label>
+
+              <v-select
+                id="grade"
+                v-model="form.grade_id"
+                :items="$store.state.grades.grades"
+                item-text="name"
+                item-value="id"
+                label="مقطع تحصیلی خود را انتخاب کنید"
+                :rules="[(v) => required(v, 'مقطع تحصیلی')]"
+                outlined
+              ></v-select>
+            </v-col>
+
+            <v-col cols="12" md="6">
+              <label for="gender" class="subtitle-1 gray--text text--darken-2">
+                جنسیت
+              </label>
+
+              <v-select
+                id="gender"
+                v-model="form.gender_id"
+                :items="$store.state.genders.genders"
+                item-text="name"
+                item-value="id"
+                label="جنسیت خود را انتخاب کنید"
+                :rules="[(v) => required(v, 'جنسیت')]"
+                outlined
+              ></v-select>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12" md="6">
+              <label
+                for="province"
+                class="subtitle-1 gray--text text--darken-2"
+              >
+                استان
+              </label>
+
+              <v-autocomplete
+                id="province"
+                v-model="province_id"
+                :items="$store.state.provinces.provinces"
+                item-text="title"
+                no-data-text="استانی پیدا نشد."
+                item-value="id"
+                :rules="[(v) => required(v, 'استان')]"
+                label="استان خود را انتخاب کنید"
+                outlined
+              ></v-autocomplete>
+            </v-col>
+
+            <v-col cols="12" md="6">
+              <label for="shahr" class="subtitle-1 gray--text text--darken-2">
+                شهر
+              </label>
+
+              <v-autocomplete
+                id="shahr"
+                v-model="form.shahr_id"
+                :items="myCities"
+                no-data-text="شهری پیدا نشد."
+                item-text="title"
+                item-value="id"
+                :rules="[(v) => required(v, 'شهر')]"
+                label="شهر خود را انتخاب کنید (اول استان را انتخاب کنید)."
+                outlined
+              ></v-autocomplete>
             </v-col>
           </v-row>
 
@@ -87,17 +179,21 @@
 const required = (v, name) => !!v || `فیلد ${name} الزامیست.`
 
 export default {
-  data: () => ({
-    isError: false,
-    form: {
-      first_name: '',
-      last_name: '',
-      address: '',
-      major_id: 0,
-      gender_id: 0,
-      shahr_id: '',
-    },
-  }),
+  data() {
+    return {
+      isError: false,
+      province_id: null,
+      form: {
+        first_name: '',
+        last_name: '',
+        address: '',
+        major_id: 0,
+        gender_id: 0,
+        shahr_id: '',
+      },
+      myCities: [],
+    }
+  },
   async fetch() {
     try {
       await this.$store.dispatch('getForm')
@@ -112,9 +208,27 @@ export default {
         gender_id,
         shahr_id,
       }
+
+      // this.myCities = this.$store.state.cities.cities
     } catch (error) {
       this.isError = true
     }
+  },
+  watch: {
+    province_id(newValue) {
+      if (!newValue) {
+        this.myCities = this.$store.state.cities.cities
+      } else {
+        this.myCities = this.$store.state.cities.cities.filter(
+          (city) => city?.province?.id === Number(newValue)
+        )
+      }
+    },
+    // 'form.shahr_id'(newValue) {
+    //   this.province_id = this.$store.state.cities.cities.find(
+    //     (city) => city.id === newValue
+    //   )?.province?.id
+    // },
   },
   methods: {
     handleSubmit() {},
